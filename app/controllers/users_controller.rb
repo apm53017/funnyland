ac701bb34db188d795ae14e4d808800a39a56fad
EC2 Default User <ec2-user@ip-172-31-33-153.ap-northeast-1.compute.internal>
2025-08-23 04:13:03 +0000

    4nd commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   before_action :ensure_correct_user, only: [:edit, :update, :destroy]
   
   def index
-    @users = User.all
+    @users = User.page(params[:page])
     @user = current_user
   end
 
